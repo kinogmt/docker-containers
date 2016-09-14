ecfedda536f104332e7f07a50945bfbf800a903f
Satoshi Kinoshita <kino@cloudian.com>
2016-09-14 14:37:09 +0900

    update for version2 of compose file

--- centos-ssh/hosts.sh
+++ centos-ssh/hosts.sh
@@ -3,6 +3,6 @@ NN=$(docker ps |grep w_centos|wc -l)
 
 # IP addresses of workers
 for i in $(seq 1 $NN); do
-    IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' w_centos_$i`
+    IP=`docker inspect --format '{{.NetworkSettings.Networks.w_net1.IPAddress}}' w_centos_$i`
     echo $IP c$i.localdomain c$i
 done
