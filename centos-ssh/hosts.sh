22e1eba788b122b33ea15cb9aa74629aa37773b1
Satoshi Kinoshita <kino@cloudian.com>
2016-09-27 10:23:53 +0900

    update centos-ssh/hosts.sh for master container

--- centos-ssh/hosts.sh
+++ centos-ssh/hosts.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-NN=$(docker ps |grep w_centos|wc -l)
+: ${PROJ:=w}
+: ${SERVICE:=centos}
+: ${SERVICEM:=centos_m}
+
+CONTS=$(docker ps --format "{{.Names}}"|grep ${PROJ}_${SERVICE}_ \
+               |grep -v $SERVICEM |sort -V)
+CONTM=$(docker ps --format "{{.Names}}"|grep ${PROJ}_${SERVICEM}_)
 
 # IP addresses of workers
-for i in $(seq 1 $NN); do
-    IP=`docker inspect --format '{{.NetworkSettings.Networks.w_net1.IPAddress}}' w_centos_$i`
+i=1
+for CONT in $CONTM $CONTS; do
+    IP=`docker inspect --format '{{.NetworkSettings.Networks.w_net1.IPAddress}}' $CONT`
     echo $IP c$i.localdomain c$i
+    i=$(( i + 1 ))
 done
